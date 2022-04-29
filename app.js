8781e9db843f71f59ff96f791da36da5d41e06d6
dakin4 <davekng0001@gmail.com>
2022-04-29 01:07:44 -0400

    added html file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Home");
+    res.sendFile(path.join(__dirname, './test.html'));
 })
 
 app.listen(port,()=>{
